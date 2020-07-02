433952db32e839a02f02539c1da451ca738387ad
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2020-07-02 06:27:44 -0700

    Testing two slightly updated versions of...
    shumway, just for fun

---
+++ testing/shumway-Breton/src/shell/references.ts
@@ -0,0 +1,7 @@
+///<reference path='domstubs.ts' />
+///<reference path='module.ts' />
+///<reference path='unit.ts' />
+///<reference path='fuzz.ts' />
+///<reference path='microTaskQueue.ts' />
+///<reference path='playerservices.ts' />
+///<reference path='shell.ts' />
